d52047b8a76fd97e69bf2d7e9a8db5cdad880b61
majunbao <mjb410641829@163.com>
2021-01-16 18:00:16 +0800

    22

---
+++ src/components/ly-title-bar.js
@@ -0,0 +1,9 @@
+Vue.component('ly-nav', {
+  template: `
+    <nav class="ly-title-wrapper">
+      <span class="ly-title-left"></span>
+      <span class="ly-title-center">瓜分百万大奖</span>
+      <span class="ly-title-right"></span>
+    </nav>
+    `
+});
---
+++ src/index.js
@@ -0,0 +1,1 @@
+import './components/ly-title-bar';
